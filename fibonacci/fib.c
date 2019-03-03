f4025c637f202809b50a4ef3b91f043c6e043d73
Dawid Layne <klatkiewiczdawid@gmail.com>
2019-03-03 18:50:34 -0500

    Fibonacci to N

---
+++ fibonacci/fib.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * Enter a number and have the program generate the Fibonacci
+ * sequence to that number or to the Nth number.
+ */
+
+int main() {
+
+   int lim;
+
+   printf("Enter a number: ");
+   scanf("%d", &lim);
+
+   unsigned long long a = 0;
+   unsigned long long b = 1;
+   unsigned long long n = 0;
+
+   for (int i = 0; i < lim; i++) {
+
+      n = a + b;
+      a = b;
+      b = n;
+
+      printf("%llu\n", n);
+
+   }
+
+   return 0;
+}
