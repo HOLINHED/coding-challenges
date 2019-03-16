b75648f96825ec172fb4211f4f0bf98d5784f9d6
Dawid Layne <klatkiewiczdawid@gmail.com>
2019-03-16 12:09:09 -0400

    Added comments to textbasedtictactoe

--- texttictactoe/Board.cs
+++ texttictactoe/Board.cs
@@ -12,56 +12,82 @@ namespace Game {
 
          dim = size;
 
+         // initializes the state
          state = new char[dim, dim];
 
+         // sets each spot in the array to an empty charater ' ' 
          for (int y = 0; y < 3; y++)
             for (int x = 0; x < 3; x++)
                state[x, y] = ' ';
 
+         // sets size to 0. this tracks how many moves have been made to the board
+         // so far.
          size = 0;
       }
 
+      // Makes a move on the board. Does not check wheter the move is valid
       public void makePlay(int x, int y, char symbol) {
          state[y, x] = symbol;
          size += 1;
       }
 
+      // Checks whether a specific move is valid. A move is valid when that spot
+      // contains an empty character
       public Boolean isValid(int x, int y) {
          return state[y,x] == ' ';
       }
 
+      // checks whether the board is in a winning state for that specific symbol
       public Boolean checkWinner(char symbol) {
 
-         int bToken = 0, fToken = 0;
+         // sets the size for cleaner code
          int size = dim;
+         
+         // sets trackers for diagonals. don't reset because there are 
+         // only two setss
+         int bToken = 0, fToken = 0;
 
+         // iterates through each row
          for (int y = 0; y < size; y++) {
 
+            // sets trackers for rows and columns. Reset because there are multiple rows
+            // and columns
             int xToken = 0, yToken = 0;
 
+            // checks forward diagonal
             if (state[y, y] == symbol) fToken += 1;
 
+            // inverses diagonal digonal X direction
             int r = (size - 1) - y;
+
+            // checks backward diagonal
             if (state[r, y] == symbol) bToken += 1;
 
+            // checks each column / row
             for (int x = 0; x < size; x++) {
 
+               // checks row
                if (state[x, y] == symbol) xToken += 1;
 
+               // checks column
                if (state[y, x] == symbol) yToken += 1;
             }
 
+            // if any trackers are equal to the size, that mean the board is in a winning state
             if (bToken == size || fToken == size || xToken == size || yToken == size)
                return true;
          }
 
+         // if passes through loop without returning, the board is not in a winning state
          return false;
       }
 
+      // checks wheter the board is full or not
       public Boolean isFull() {
          return size == dim * dim;
       }
 
+      // prints out the state of the board
       public String toString() {
 
          String board = "";
--- texttictactoe/TicTacToe.cs
+++ texttictactoe/TicTacToe.cs
@@ -12,24 +12,32 @@ namespace Game {
       private String winner;
       
       public TicTacToe() {
+         
+         // Initializes board
          board = new Board(3);
 
+         // Sets both turn and tunning to true
          turn = running = true;
       }
 
       public static void Main() {
          
+         // Makes new game instance
          TicTacToe game = new TicTacToe();
 
+         // Main game loop
          while (game.running) {
 
+            // Checks which player playing TRUE if X false if O
             char playing = game.turn ? 'X' : 'O';
 
+            // Shows current state of board and which player's turn it is
             Console.WriteLine("\n" + playing + " TURN!\n");
             Console.WriteLine(game.board.toString());
 
             int x = -1;
 
+            // Gets new X position from player while their input is not valid
             while (x < 0 || x > 3) {
                Console.Write("Enter x position: ");
                x = Convert.ToInt32(Console.ReadLine()) - 1;
@@ -37,30 +45,44 @@ namespace Game {
 
             int y = -1;
 
+            // Gets new Y position from player while their input is not valid
             while (y < 0 || y > 3) {
                Console.Write("Enter y position: ");
                y = Convert.ToInt32(Console.ReadLine()) - 1;
             }
 
+            // If the move they want to make is not valid, don't make the move. And that
+            // player has to go again when the game loops.
             if (game.board.isValid(x,y)) {
+               
+               // change who's turn it is
                game.turn = !game.turn;
 
+               // make the move
                game.board.makePlay(x,y,playing);
             
-               if (game.board.isFull()) { 
-                  game.running = false;
-                  game.winner = "NONE";
-               }
-
+               // check if the move the player just made was a winning move, if so, stop the game
+               // loop and set the winner.
                if (game.board.checkWinner(playing)) {
                   game.running = false;
                   game.winner = playing.ToString();
                }
+               
+               // if board is full and there was no winner, stop the game loop and set winner 
+               // to NONE
+               if (game.board.isFull() && game.running) { 
+                  game.running = false;
+                  game.winner = "NONE";
+               }
+
+               
             }
          }
 
+         // Prints final state of board
          Console.WriteLine("\n" + game.board.toString());
 
+         // Prints winner
          Console.WriteLine("WINNER: " + game.winner);
          
       }
