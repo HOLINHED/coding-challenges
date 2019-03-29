0b5826fda3c16ed0c24de92cfa8eed8c87e627e4
Dawid Layne <klatkiewiczdawid@gmail.com>
2019-03-29 12:21:47 -0400

    Made better bubblesort

--- bubblesort/Main.java
+++ bubblesort/Main.java
@@ -9,29 +9,23 @@ public class Main {
 
      public static void main(String[] args){
         
-        final int n = (int) Math.floor(Math.random() * 20); 
+        final int n = (int) Math.floor(Math.random() * (20 - 10) + 10); 
         int[] nums = new int[n];
 
         for (int i = 0; i < nums.length; i++)
             nums[i] = (int) Math.floor(Math.random() * 255);
         
         System.out.println(Arrays.toString(nums));
-        
-        int c;
-        
-        do {
-            c = 0;
-            
-            for (int i = 1; i < nums.length; i++) {
-                if (nums[i - 1] > nums[i]) {
-                    final int temp  = nums[i - 1];
-                    nums[i - 1] = nums[i];
-                    nums[i] = temp;
-                    c += 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    final int temp = nums[j + 1];
+                    nums[j + 1] = nums[j];
+                    nums[j] = temp;
                 }
             }
-
-        } while (c > 0);
+        }
         
         System.out.println(Arrays.toString(nums));
         
